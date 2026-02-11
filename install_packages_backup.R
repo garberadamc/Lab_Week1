73fb3960ab58b4366cf2b56d3051f00618a4ad44
Adam Garber <garberadamc@gmail.com>
2026-02-11 09:50:49 -0800

    update26

---
+++ install_packages_backup.R
@@ -0,0 +1,26 @@
+
+
+# Install Packages 
+
+install.packages("remotes")
+remotes::install_github("MatthewBJane/ThemePark")
+
+# install.packages('devtools')
+# devtools::install_github('bbc/bbplot')
+
+install.packages("tidyverse")
+install.packages("here")
+install.packages("janitor")
+install.packages("ggthemes")
+install.packages("viridis")
+install.packages("ThemePark")
+
+
+# Load Packages = "Libraries"
+
+library(tidyverse)
+library(here)
+library(janitor)
+library(ggthemes)
+library(viridis)
+library(ThemePark)
